11a736971760c3565db5b2e7a9e4cff606f81da1
Toni <tonirachev92@gmail.com>
2025-08-23 20:12:01 +0300

    feat: add About component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,11 @@
+import About from "./components/about";
 import Hero from "./components/hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="space-y-20">
       <Hero />
+      <About />
     </div>
   );
 }
