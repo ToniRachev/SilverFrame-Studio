fac1355c64485c1f0650db27bc91e98b95f5268d
Toni <tonirachev92@gmail.com>
2025-08-25 23:06:04 +0300

    feat: add ServicesHeading component

---
+++ app/(home)/components/services/components/services-heading.tsx
@@ -0,0 +1,11 @@
+import TextMask from "@/components/text-mask";
+
+export default function ServicesHeading() {
+    return (
+        <TextMask>
+            <h2>Bringing</h2>
+            <h2>Stories to Life</h2>
+            <p className="max-w-prose">From script to screen, we specialize in crafting cinematic experiences that captivate, inspire, and resonate with audiences worldwide</p>
+        </TextMask>
+    )
+}
