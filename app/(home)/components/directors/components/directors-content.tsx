b190986107500676c4514d6356f63590a45db141
Toni <tonirachev92@gmail.com>
2025-08-27 11:53:04 +0300

    feat: refactor DirectorsContent to use reusable Title

--- app/(home)/components/directors/components/directors-content.tsx
+++ app/(home)/components/directors/components/directors-content.tsx
@@ -1,7 +1,21 @@
+import clsx from "clsx";
+
+interface TitleProps {
+    text: string;
+    className?: string;
+}
+
+const Title = ({ text, className }: Readonly<TitleProps>) => {
+    return (
+        <h1 className={clsx("max-w-[15ch] !text-[8em] !m-0", className)}>{text}</h1>
+    )
+}
+
 export default function DirectorsContent() {
     return (
         <div className="mt-4">
-            <h1 className="max-w-[15ch] !text-[8em]">The Storytellers behind the lens</h1>
+            <Title text="The Storytellers" />
+            <Title text="Behind the Lens" className="text-decor" />
         </div>
     )
 }
