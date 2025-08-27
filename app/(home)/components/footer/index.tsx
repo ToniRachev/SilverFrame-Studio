0882614778887fc6cb2f54cb33a0772a6127f20a
Toni <tonirachev92@gmail.com>
2025-08-27 13:34:33 +0300

    feat: replace Contact component with new Footer component featuring GSAP animations

--- app/(home)/components/footer/index.tsx
+++ app/(home)/components/footer/index.tsx
@@ -7,7 +7,7 @@ import { useRef } from "react";
 
 import SectionContainer from "@/components/section-container";
 
-export default function Contact() {
+export default function Footer() {
     const containerRef = useRef<HTMLDivElement>(null);
     const imageWrapperRef = useRef<HTMLDivElement>(null);
     const textRef = useRef<HTMLHeadingElement>(null);
@@ -65,24 +65,26 @@ export default function Contact() {
 
 
     return (
-        <SectionContainer className="">
-            <div ref={containerRef} className="w-full h-screen flex flex-col justify-center items-center">
-                <h1 ref={textRef} className="!m-0 !indent-0">Let&apos;s</h1>
+        <footer>
+            <SectionContainer className="">
+                <div ref={containerRef} className="w-full h-screen flex flex-col justify-center items-center">
+                    <h1 ref={textRef} className="!m-0 !indent-0">Let&apos;s</h1>
 
-                <div ref={imageWrapperRef} className="relative w-[30vw] h-[30vh] origin-center">
-                    <Image src="/home/awards/studio-award.jpg" alt="Contact 1" fill className="object-cover object-[50%_90%]" />
+                    <div ref={imageWrapperRef} className="relative w-[30vw] h-[30vh] origin-center">
+                        <Image src="/home/awards/studio-award.jpg" alt="Contact 1" fill className="object-cover object-[50%_90%]" />
 
-                    <div ref={caption1Ref} className="absolute -left-1/3 top-1/2 -translate-y-1/2 opacity-0">
-                        <p className="italic">Stories worth telling</p>
+                        <div ref={caption1Ref} className="absolute -left-1/3 top-1/2 -translate-y-1/2 opacity-0">
+                            <p className="italic">Stories worth telling</p>
+                        </div>
+
+                        <div ref={caption2Ref} className="absolute -right-1/3 top-1/2 -translate-y-1/2 flex gap-10 justify-end opacity-0">
+                            <p className="italic">Films worth making</p>
+                        </div>
                     </div>
 
-                    <div ref={caption2Ref} className="absolute -right-1/3 top-1/2 -translate-y-1/2 flex gap-10 justify-end opacity-0">
-                        <p className="italic">Films worth making</p>
-                    </div>
+                    <h1 ref={text2Ref} className="!indent-0">Create</h1>
                 </div>
-
-                <h1 ref={text2Ref} className="!indent-0">Create</h1>
-            </div>
-        </SectionContainer>
+            </SectionContainer>
+        </footer>
     )
 }
